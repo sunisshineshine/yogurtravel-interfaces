e4a1a236778cc938c08bd992a90c0de60ca79a6f
pandajiny <astic1764@gmail.com>
2020-09-24 15:33:46 +0900

    plan migration

--- schemas/plan.d.ts
+++ schemas/plan.d.ts
@@ -2,7 +2,10 @@ interface Plan extends FirebaseDocumentObject {
   uid: string;
   title: string;
   createTime: FirebaseFirestore.Timestamp;
-  places: string[];
+  planItems: firebase.firestore.DocumentReference[];
+  // old
+  // places => items 20200924
+  places?: string[];
 }
 
 interface CreatePlanRequest {
